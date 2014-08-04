44b91d77f7b325e25aaf515f806462c7a5315fa1
rapoulson <rebecca.poulson@gmail.com>
2014-08-04 17:18:37 -0400

    cattle picture

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 (function() {
   function appStart() {
-    alert("Application started!");
+    TT.api.get('v1/me').done(fetchStoreProducts).fail(genericError);
   }
 
   function genericError() {
     console.error('Something went wrong');
+  }
+
+  function fetchStoreProducts(store) {
+  TT.api.get('v1/stores/' + store.id + '/products')
+    .done(listProductImages)
+    .fail(genericError);
+  }
+
+  function listProductImages(products) {
+  var imageContainer = $('.images');
+
+  for (var i = 0; i < products.length; i++) {
+    var product = products[i];
+    $('<img>')
+      .attr('src', product.images[0].sizes['100'])
+      .appendTo(imageContainer);
+    }
   }
 
   TT.native.init()
